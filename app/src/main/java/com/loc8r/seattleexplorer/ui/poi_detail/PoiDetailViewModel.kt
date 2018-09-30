ed49d430b9592308885ba5fb53f75cbaebe5cb04
szaske <github@zaske.com>
2018-09-29 17:12:57 -0700

    Creating the Domain layer

--- app/src/main/java/com/loc8r/seattleexplorer/ui/poi_detail/PoiDetailViewModel.kt
+++ app/src/main/java/com/loc8r/seattleexplorer/ui/poi_detail/PoiDetailViewModel.kt
@@ -1,12 +1,14 @@
 package com.loc8r.seattleexplorer.ui.poi_detail
 
 import android.arch.lifecycle.ViewModel
+import com.loc8r.seattleexplorer.domain.GetPois
 
 class PoiDetailViewModel : ViewModel() {
 
-    val detailMessage: String = "Steve was here"
+    // Step #5 changes.  I'm instantiating this use case class in the domain layer and getting my info from that.
+    private val getPois = GetPois()
 
     fun getMessage(): String {
-        return detailMessage
+        return getPois.getPoiDetailMessage()
     }
 }
