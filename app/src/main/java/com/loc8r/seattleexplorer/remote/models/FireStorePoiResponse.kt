700288689207b772e2a4b292bffac37ca30c3d8e
szaske <github@zaske.com>
2018-10-12 16:35:28 -0700

    Building the Firebase Remote Module

---
+++ app/src/main/java/com/loc8r/seattleexplorer/remote/models/FireStorePoiResponse.kt
@@ -0,0 +1,29 @@
+package com.loc8r.seattleexplorer.remote.models
+
+import com.loc8r.seattleexplorer.repository.models.PoiRepository
+
+data class FireStorePoiResponse(var id: String = "",
+                                val name: String = "",
+                                val description: String = "",
+                                val img_url: String = "",
+                                val latitude: Double = 0.0,
+                                val longitude: Double = 0.0,
+                                val imgFocalpointX: Double = 0.0,
+                                val imgFocalpointY: Double = 0.0,
+                                val collection: String = "",
+                                val collectionPosition: Int = 0,
+                                val release: Int = 0,
+                                val stampText: String = ""){
+    fun mapToRepository() = PoiRepository(id,
+            name,
+            description,
+            img_url,
+            latitude.toDouble(),
+            longitude.toDouble(),
+            imgFocalpointX.toDouble(),
+            imgFocalpointY.toDouble(),
+            collection,
+            collectionPosition.toInt(),
+            release.toInt(),
+            stampText)
+}
