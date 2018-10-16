8a33ff96f48e6894aa2040ea457f306725ebce54
szaske <github@zaske.com>
2018-10-16 10:54:46 -0700

    Adding a RecyclerView and Adapter

--- app/src/main/java/com/loc8r/seattleexplorer/presentation/poiList/PoiListFragment.kt
+++ app/src/main/java/com/loc8r/seattleexplorer/presentation/poiList/PoiListFragment.kt
@@ -1,3 +1,13 @@
+/**
+ * The Fragment acts as the View in the Model, View, ViewModel design pattern.  It is responsible for
+ *
+ * 1. Instantiating the View and the items that make up the view.
+ * 2. Attaching the viewmodel data to the view components
+ *
+ * Ideally the view would be made up completely of XML.  Data-binding seems to be the preferred method of accomplishing #2 above, but the approach doesn't
+ *
+ */
+
 package com.loc8r.seattleexplorer.presentation.poiList
 
 import android.arch.lifecycle.Observer
@@ -6,6 +16,8 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +26,6 @@ import com.loc8r.seattleexplorer.di.ViewModelFactory
 import com.loc8r.seattleexplorer.presentation.interfaces.OnFragmentInteractionListener
 import com.loc8r.seattleexplorer.presentation.models.PoiPresentation
 import dagger.android.support.AndroidSupportInjection
-import kotlinx.android.synthetic.main.poi_list_fragment.*
 import javax.inject.Inject
 
 
@@ -43,24 +54,11 @@ class PoiListFragment : Fragment() {
     @Inject lateinit var viewModelFactory: ViewModelFactory
     private lateinit var poiListViewModel: PoiListViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // RecyclerView variables
+    private var mRecyclerView: RecyclerView? = null
+    @Inject lateinit var poiListAdapter: PoiListAdapter
+    private var mLayoutManager: RecyclerView.LayoutManager? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.poi_list_fragment, container, false)
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
 
     override fun onAttach(context: Context) {
         // Here's I'm kicking off the injection process for the Fragment
@@ -72,6 +70,54 @@ class PoiListFragment : Fragment() {
             throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
         }
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val rootView = inflater.inflate(R.layout.poi_list_fragment,container,false)
+
+        // Configure the RecyclerView
+        mRecyclerView = rootView.findViewById(R.id.recycler_poisList)
+        mLayoutManager = LinearLayoutManager(this.context)
+        mRecyclerView?.layoutManager = mLayoutManager
+        mRecyclerView?.setHasFixedSize(true)
+        mRecyclerView?.adapter = poiListAdapter
+
+        // Inflate the layout for this fragment
+        return rootView
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        // assigning the viewmodel according to the map in the viewModelFactory
+        poiListViewModel = ViewModelProviders.of(this,viewModelFactory).get(PoiListViewModel::class.java)
+
+        // This Requests from the Presentation layer the liveData stream which is nothing
+        poiListViewModel.getAllPois().observe(this,
+                Observer<List<PoiPresentation>> { it ->
+
+                    // let is used to test against null
+                    it?.let {
+                        poiListAdapter.pois = it
+                        poiListAdapter.notifyDataSetChanged()
+                    }
+                })
+    }
+
 
     override fun onDetach() {
         super.onDetach()
@@ -97,22 +143,4 @@ class PoiListFragment : Fragment() {
                     }
                 }
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        // assigning the viewmodel according to the map in the viewModelFactory
-        poiListViewModel = ViewModelProviders.of(this,viewModelFactory).get(PoiListViewModel::class.java)
-
-        // This Requests from the Presentation layer the liveData stream which is nothing
-        poiListViewModel.getAllPois().observe(this,
-                Observer<List<PoiPresentation>> { it ->
-
-                    // let is used to test against null
-                    it?.let {
-                        poiListFragment_txt.text = it[0].name
-                    }
-                })
-    }
-
 }
